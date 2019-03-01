f70555d316902fe4b6a4b2953797868294c4c358
PitCoder <overlord.lae@gmail.com>
2019-03-01 00:55:55 -0600

    ANU Cipher Implementation 64/128

--- Block Ciphers/anu2.py
+++ Block Ciphers/anu2.py
@@ -18,13 +18,20 @@ For i=0 to 24 do
 
     Increment i by 1
 '''
-from bitstring import BitArray
+from bitstring import BitArray, BitStream
 
 # Definition of constants
-BLOCK_SIZE = 4
+BOX_SIZE = 4
+BLOCK_SIZE = 64
+HALVE_BLOCK = int(BLOCK_SIZE / 2)
 KEY_SIZE = 128  # For ANU-II can be 80/128
-KEY_HALF = int(KEY_SIZE/2)
-KEY_QUARTER = int(KEY_HALF/2)
+KEY_HALF = int(KEY_SIZE / 2)
+KEY_QUARTER = int(KEY_HALF / 2)
+
+MIRROR_32_BITS = BitArray('0xFFFFFFFF')
+MIRROR_64_BITS = BitArray('0xFFFFFFFFFFFFFFFF')
+MIRROR_80_BITS = BitArray('0xFFFFFFFFFFFFFFFFFFFF')
+MIRROR_128_BITS = BitArray('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
 
 # Defintion of tables (S-Box for ANU II)
 SUBSTITUTION_TABLE = ['1110', '0100', '1011', '0001',
@@ -33,19 +40,27 @@ SUBSTITUTION_TABLE = ['1110', '0100', '1011', '0001',
                       '1000', '0101', '0011', '0110']
 
 
+def left_rotation(vector, positions, int_blocks, mirror):
+    return (vector << positions) | ((vector >> (int_blocks-positions)) & mirror)
+
+
+def right_rotation(vector, positions, int_blocks, mirror):
+    return (vector >> positions) | ((vector << (int_blocks-positions)) & mirror)
+
+
 def sustitution_box(data_block):
     bit_block = str(data_block.bin)
     sustitution_block = []
 
-    for i in range(0, len(bit_block), BLOCK_SIZE):
-        sustitution_block.append(SUBSTITUTION_TABLE[int(bit_block[0 + i: BLOCK_SIZE + i], 2)])
+    for i in range(0, len(bit_block), BOX_SIZE):
+        sustitution_block.append(SUBSTITUTION_TABLE[int(bit_block[0 + i: BOX_SIZE + i], 2)])
 
-    return BitArray('0b', ''.join(sustitution_block[:]))
+    return BitArray('0b' + ''.join(sustitution_block[:]))
 
 
 def key_update(key, round):
     # Left circular shift by 13
-    update = key << 13
+    update = left_rotation(key, 13, KEY_SIZE, MIRROR_128_BITS)
     # S-Box of LSB 8-bits
     # KEY[K3,K2,K1,K0] = S[K3,K2,K1,K0]
     # KEY[K7,K6,K5,K4] = S[K7,K6,K5,K4]
@@ -62,42 +77,41 @@ if __name__ == '__main__':
     # Define number of rounds
     NO_ROUNDS = 25
 
-    #Test vector of ANU-II(128 bit key)
-    #Plaintext      Key         Ciphertext
-    #00000000 0000000000000000   5aac94c2
-    #00000000 0000000000000000   b4dcb8c6
-    #=====================================
-    #00000000 ffffffffffffffff   0d5abbc9
-    #00000000 ffffffffffffffff   d572802a
-    #=====================================
-    #ffffffff 0000000000000000   125d2f37
-    #ffffffff 0000000000000000   b4dcb8c6
-
+    # Test vector of ANU-II(128 bit key)
+    # Plaintext      Key         Ciphertext
+    # 00000000 0000000000000000   5aac94c2
+    # 00000000 0000000000000000   b4dcb8c6
+    # =====================================
+    # 00000000 ffffffffffffffff   0d5abbc9
+    # 00000000 ffffffffffffffff   d572802a
+    # =====================================
+    # ffffffff 0000000000000000   125d2f37
+    # ffffffff 0000000000000000   2b2cd748
 
     plaintext = BitArray('0xffffffffffffffff')
-    key = BitArray('0xffffffffffffffffffffffffffffffff')
+    key = BitArray('0x00000000000000000000000000000000')
 
     # Spliting the block data into halves
     p_msb = plaintext[:32]  # Most significant bits from the block p
     p_lsb = plaintext[32:]  # Less significant bits from the block p
 
-    for i in range(0, 24):
+    for i in range(0, NO_ROUNDS):
+        # Variable copies
         # Apply S-Box at p_msb
         p_msb = sustitution_box(p_msb)
         # XOR key, S-Box output and right circularity shifted
-        p_msb = p_msb ^ key[KEY_HALF:KEY_HALF+KEY_QUARTER] ^ (p_lsb >> 3)
+        p_msb = p_msb ^ key[KEY_HALF + KEY_QUARTER:] ^ right_rotation(p_lsb, 3, HALVE_BLOCK, MIRROR_32_BITS)
         # XOR key, P_LSBi and left circularity shifted
-        p_lsb = p_lsb ^ key[KEY_HALF+KEY_QUARTER:] ^ (p_msb << 10)
+        p_lsb = p_lsb ^ key[KEY_HALF:KEY_HALF + KEY_QUARTER] ^ left_rotation(p_msb, 10, HALVE_BLOCK, MIRROR_32_BITS)
         # Swap
-        temp = p_msb;
-        p_msb = p_lsb;
-        p_lsb = temp;
+        temp = p_msb[:]
+        p_msb = p_lsb
+        p_lsb = temp
         # Key update
-        key = key_update(key, round)
+        key = key_update(key, i)
 
-    c_msb = p_msb
-    c_lsb = p_lsb
+    ciphertext = p_msb + p_lsb
 
-    print("ciphertext: ", (c_msb + c_lsb).hex)
-    print("ciphertext: ", (c_msb + c_lsb).hex)
-    print("ciphertext: ", (c_msb+c_lsb).hex)
+    print("plaintext: ", plaintext.hex)
+    print("key: ", key.hex)
+    print("ciphertext: ", ciphertext.hex)
